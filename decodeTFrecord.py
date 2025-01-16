17bf05eab53580e64a74494fc8ca41f629649ef9
Alexaqua7 <sdh582@naver.com>
2025-01-16 04:56:20 +0000

    :hammer: #5 Frame-level Dataset에서도 작동하도록 수정정

--- decodeTFrecord.py
+++ decodeTFrecord.py
@@ -12,6 +12,7 @@ class YoutubeSegmentDataset(torch.utils.data.IterableDataset):
     Youtube-8M-Segment Dataset을 처리할 수 있는 데이터셋 클래스입니다.
     
     args
+    segm: "seg" 또는 "frame". Youtube-8M-Segment Dataset인 경우 seg, General frame level Youtube-8M Dataset인 경우 frame으로 설정하세요
     file_paths: .tfrecord 형식의 Youtube-8M-Segment Dataset의 데이터 경로를 설정하세요
     seed(Optional): 시드 설정을 위해 입력받으며, 입력하지 않을 시 Default 42로 설정됩니다
     debug(Optional): 디버깅을 수행하려면 True로 설정하세요. Default: False
@@ -20,11 +21,12 @@ class YoutubeSegmentDataset(torch.utils.data.IterableDataset):
     max_examples (Optional): 데이터 셋 내에서 데이터의 수량을 제한하고자 할 때 사용합니다. None으로 설정 시, 제한하지 않고 전체를 이용하겠다는 의미입니다. Default: None
     offset (Optional): Video feature 앞 뒤로 얼마나 padding을 적용할 것인지를 설정합니다. Default: 0
     """
-    def __init__(self, file_paths, seed=42, debug=False,
+    def __init__(self, segm, file_paths, seed=42, debug=False,
                  vocab_path="./data/segment_vocabulary.csv",
                  epochs=1, max_examples=None, offset=0):
         super(YoutubeSegmentDataset).__init__()
-        self.file_paths = list(glob.glob(str(file_paths))) if "*" in file_paths else file_paths
+        self.segm = "frame" if segm=='frame' else "seg"
+        self.file_paths = list(glob.glob(str(file_paths))) if "*" in file_paths else [file_paths]
         self.seed = seed
         self.debug = debug
         self.max_examples = max_examples
@@ -57,31 +59,10 @@ class YoutubeSegmentDataset(torch.utils.data.IterableDataset):
         tmp = example.FromString(row.numpy())
         context, video_features = tmp.context, tmp.feature_lists
 
+        # 공통 feature
         vid_labels = list(context.feature['labels'].int64_list.value)
         vid_labels_encoded = set([self.label_mapping[x] for x in vid_labels if x in self.label_mapping])
-
-        # 뽑을 feature들 따로 저장
-        segment_labels = np.array(context.feature['segment_labels'].int64_list.value).astype("int64")
-        segment_start_times = np.array(context.feature['segment_start_times'].int64_list.value)
-        segment_scores = np.array(context.feature['segment_scores'].float_list.value).astype("int64")
         vid = context.feature['id'].bytes_list.value[0].decode('utf8')
-
-
-        # Transform label
-        segment_labels = np.array([self.label_mapping[x] for x in segment_labels])
-
-        # Dummy Data. Segment Label을 인덱스->영문으로 변환하여 저장. 사용 중이지는 않음
-        segment_labels_name = np.array(list(set([self.name_mapping[x] for x in segment_labels])))
-
-        # Negative Mining (우리 Task에서 굳이 필요할까?)
-        if self.debug:
-            if not vid_labels_encoded: # Set이 공집합이다 == 인코딩이 잘 되지 않았을 것
-                print(segment_labels, vid_labels)
-            else:
-                print("Passed")
-        negative_mask = np.zeros(1000, dtype=int)
-        # segment_labels에 없는 레이블을 1로 지정함
-        negative_mask[np.array(list(set(range(1000)) - vid_labels_encoded - set(segment_labels)))] = 1
 
         # Frames. Shape: (frames, 1024)
         tmp = video_features.feature_list['rgb'].feature
@@ -94,6 +75,31 @@ class YoutubeSegmentDataset(torch.utils.data.IterableDataset):
         # Frames + Audio Combined: (frames, 1024+128=1152) - 추후 사용 예정
         video_features = torch.from_numpy(frames)
         # video_features = torch.from_numpy(np.concatenate([frames, audio], axis=-1))
+        if self.segm == 'frame':
+            return video_features
+
+        # segment에만 존재하는 feature
+        segment_labels = np.array(context.feature['segment_labels'].int64_list.value).astype("int64")
+        segment_start_times = np.array(context.feature['segment_start_times'].int64_list.value)
+        segment_scores = np.array(context.feature['segment_scores'].float_list.value).astype("int64")
+
+
+        # Transform label
+        segment_labels = np.array([self.label_mapping[x] for x in segment_labels])
+
+        # Dummy Data. Segment Label을 인덱스->영문으로 변환하여 저장. 사용 중이지는 않음
+        segment_labels_name = np.array(list(set([self.name_mapping[x] for x in segment_labels])))
+
+        # Negative Mining (우리 Task에서 굳이 필요할까?)
+        negative_mask = np.zeros(1000, dtype=int)
+        # segment_labels에 없는 레이블을 1로 지정함
+        negative_mask[np.array(list(set(range(1000)) - vid_labels_encoded - set(segment_labels)))] = 1
+
+        if self.debug:
+            if not vid_labels_encoded: # Set이 공집합이다 == 인코딩이 잘 되지 않았을 것
+                print(segment_labels, vid_labels)
+            else:
+                print("Passed")
 
         if self.debug:
             print(f"http://data.yt8m.org/2/j/i/{vid[:2]}/{vid}.js")
@@ -124,19 +130,24 @@ class YoutubeSegmentDataset(torch.utils.data.IterableDataset):
         # Shape: (n_segments * (5 + self.offset * 2), 1152) -> (n_segments, 5 + self.offset * 2, 1152)
         # 이 코드 정확히는 이해 안됨
         segments = torch.index_select(video_features_padded, 0, indices.view(-1)).view(indices.size(0), 5 + self.offset * 2, -1)
-
-        return video_features, segments, torch.from_numpy(segment_labels).unsqueeze(1), segment_labels_name, torch.from_numpy(segment_scores).unsqueeze(1)
+        return video_features, segments, torch.from_numpy(segment_labels).unsqueeze(1), np.expand_dims(segment_labels_name, axis=1), torch.from_numpy(segment_scores).unsqueeze(1)
 
     def _iterate_through_dataset(self, tf_dataset):
         """
         tf_dataset을 받아 데이터를 반환해주는 내부 함수
         """
         ## 임시
-        for row in tf_dataset:
-            # self.prepare_one_sample(row)를 한 번 더 감싸서 튜플 형태로 만들어 Rank를 1 이상으로 만들어 주기 위함 (안하면 Value Error - Unbatching a tensor 오류뜸)
-            video_features, segments, segment_labels, segment_labels_names, segment_scores = self.prepare_one_sample(row)
-            for segment, label, n_label, score in zip(segments, segment_labels, segment_labels_names, segment_scores):
-                yield video_features, segment, torch.cat([label, score]), n_label
+        if self.segm == 'frame':
+            for row in tf_dataset:
+                # self.prepare_one_sample(row)를 한 번 더 감싸서 튜플 형태로 만들어 Rank를 1 이상으로 만들어 주기 위함 (안하면 Value Error - Unbatching a tensor 오류뜸)
+                video_features = self.prepare_one_sample(row)
+                yield video_features
+        else:
+            for row in tf_dataset:
+                # self.prepare_one_sample(row)를 한 번 더 감싸서 튜플 형태로 만들어 Rank를 1 이상으로 만들어 주기 위함 (안하면 Value Error - Unbatching a tensor 오류뜸)
+                video_features, segments, segment_labels, segment_labels_names, segment_scores = self.prepare_one_sample(row)
+                for segment, label, n_label, score in zip(segments, segment_labels, segment_labels_names, segment_scores):
+                    yield video_features, segment, torch.cat([label, score]), n_label
 
 
     def generator(self, seed):
@@ -168,29 +179,93 @@ def collate_videos(batch, pad=0):
     for i, row in enumerate(transposed[0]):
         data[i, :len(row)] = row
         masks[i, :len(row)] = 1
-    
     # Labels
     if transposed[1][0] is None:
         return data, masks, None
     labels = torch.stack(transposed[1]).float()
     return data, masks, labels
 
-file_path = "./*.tfrecord"
-dataset = YoutubeSegmentDataset(file_paths=file_path)
-for i,(v,s,ls,nl) in enumerate(dataset):
-    print(f"--------------{i} Data --------------")
-    print(f"video_features.shape: {v.shape}")
-    print(f"segment.shape: {s.shape}")
-    print(f"label+score.shape: {ls.shape}, segment_label: {ls}")
-    print(f"named label: {nl}")
-    print("--------------------------------------")
-    if i==3:
-        break
+def collate_segments(batch, pad=0):
+    """Batch preparation.
 
-loader = DataLoader(dataset, num_workers=0, batch_size=16, collate_fn = collate_videos)
+    Pads the sequences
+    """
+    #  frames, segment, (label, score, negative_mask)
+    transposed = list(zip(*batch))
+    max_len = max((len(x) for x in transposed[0]))
+    video_data = torch.zeros(
+        (len(batch), max_len, transposed[0][0].size(-1)),
+        dtype=torch.float
+    ) + pad
+    video_masks = torch.zeros((len(batch), max_len), dtype=torch.float)
+    for i, row in enumerate(transposed[0]):
+        video_data[i, :len(row)] = row
+        video_masks[i, :len(row)] = 1
+    segments = torch.stack(transposed[1]).float()
+    labels = torch.stack(transposed[2])
+    nlabels = np.array(transposed[3])
+    return video_data, video_masks, segments, labels, nlabels
 
-for i, (data, masks, labels) in enumerate(loader):
-    print(f"NOW: {(i+1)*2}")
-    print(data.shape)
-    print(masks.shape)
-    print(labels.shape)
+def collate_frame(batch, pad=0):
+    """
+    batch: [video_features_1, video_features_2, ...]
+    video_features: Tensor of shape (frames, 1024)
+    """
+    max_len = max(x.size(0) for x in batch)
+    data = torch.zeros((len(batch), max_len, batch[0].size(-1)), dtype=torch.float) + pad
+    masks = torch.zeros((len(batch), max_len, batch[0].size(-1)), dtype=torch.float)
+    for i, row in enumerate(batch):
+        data[i, :len(row)] = row
+        masks[i, :len(row)] = 1
+
+    return data, masks
+
+
+# file_path = "./*.tfrecord"
+
+# seg 예시
+# file_path = "./validate0031.tfrecord"
+# typ = 'seg'
+
+# frame 예시
+file_path = "./trainpj.tfrecord"
+typ = 'frame'
+dataset = YoutubeSegmentDataset(typ, file_paths=file_path)
+
+if typ == 'seg':
+    print('This is the Segments rated Frame Level Dataset')
+    for i,(v,s,ls,nl) in enumerate(dataset):
+        print(f"--------------{i} Data --------------")
+        print(f"video_features.shape: {v.shape}")
+        print(f"segment.shape: {s.shape}")
+        print(f"label+score.shape: {ls.shape}, segment_label: {ls}")
+        print(f"named label: {nl}")
+        print("--------------------------------------")
+        if i==3:
+            break
+else:
+    print('This is the Frame Level Dataset')
+    for i, v in enumerate(dataset):
+        print(f"--------------{i} Data --------------")
+        print(f"video_features.shape: {v.shape}")
+        print("--------------------------------------")
+        if i==3:
+            break
+batch_size = 5
+if typ == 'frame':
+    # Frame Level Youtube-8M Dataset 예시
+    loader = DataLoader(dataset, num_workers=0, batch_size=batch_size, collate_fn = collate_frame)
+    for i, (data, masks) in enumerate(loader):
+        print(f"NOW: {(i+1)*batch_size}")
+        print(f"data.shape: {data.shape}")
+        print(f"masks.shape: {masks.shape}")
+else:
+    # Segments-Rated Frame Level Youtube-8M Dataset 예시
+    loader = DataLoader(dataset, num_workers=0, batch_size=batch_size, collate_fn = collate_segments)
+    for i, (video_data, video_masks, segments, labels, nlabels) in enumerate(loader):
+        print(f"NOW: {(i+1)*batch_size}")
+        print(f"video_data.shape: {video_data.shape}")
+        print(f"video_masks.shape: {video_masks.shape}")
+        print(f"segment.shape: {segments.shape}")
+        print(f"labels.shape: {labels.shape}")
+        print(f"nlabels.shape: {nlabels.shape}")
