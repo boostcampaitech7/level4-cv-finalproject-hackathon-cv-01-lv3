8dc4ef1eca26b3690771f84d96e7db506db47a11
Alexaqua7 <sdh582@naver.com>
2025-03-11 16:16:00 +0900

    :hammer: T2V를 Multi-modality embedding을 활용하여 수행할 수 있도록 변경

--- demo/download.py
+++ demo/download.py
@@ -1,6 +1,8 @@
 from huggingface_hub import snapshot_download
 import os
-model_name = "yibinlei/LENS-d8000"
-local_dir = './weights_yibinlei/LENS-d8000'
+# Official Repo for LongCLIP needed, plus the LongCLIP-L.pt from Huggingface
+
+tt_model_name = "snowflake/snowflake-arctic-embed-l-v2.0"
+local_dir = '../t2v/weights/weights_snowflake-arctic-embed-l-v2.0'
 os.makedirs(local_dir, exist_ok=True)
-download_dir = snapshot_download(repo_id=model_name, local_dir=local_dir)# ,ignore_patterns=["*.py"])
+download_dir = snapshot_download(repo_id=tt_model_name, local_dir=local_dir)
